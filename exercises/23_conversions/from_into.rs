a6d1df60041d4d1f1ef2555bda702b7f97ab496d
Shahab <shahab.uweng@gmail.com>
2024-04-17 10:24:59 -0400

    Final set of exercises

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -24,7 +24,6 @@ impl Default for Person {
     }
 }
 
-
 // Your task is to complete this implementation in order for the line `let p1 =
 // Person::from("Mark,20")` to compile. Please note that you'll need to parse the
 // age component into a `usize` with something like `"4".parse::<usize>()`. The
@@ -41,10 +40,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {}
+    fn from(s: &str) -> Person {
+        let details: Vec<&str> = s.split(',').collect();
+        match details.len() {
+            2 => {
+                let name = details.get(0).unwrap_or(&"");
+                if name.is_empty() {
+                    return Person::default();
+                }
+
+                let age = details.get(1).unwrap_or(&"0").parse::<usize>().unwrap_or(0);
+                if age == 0 {
+                    return Person::default();
+                }
+
+                Person {
+                    name: name.to_string(),
+                    age,
+                }
+            }
+            _ => Person::default(),
+        }
+    }
 }
 
 fn main() {
